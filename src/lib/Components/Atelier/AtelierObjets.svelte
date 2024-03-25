5cd10568ae4c9c145ed9c9fe004c1f414e562591
Thomas <hello@thomasflorentin.net>
2024-03-25 13:19:32 +0100

    Fix Objets visuels

--- src/lib/Components/Atelier/AtelierObjets.svelte
+++ src/lib/Components/Atelier/AtelierObjets.svelte
@@ -2,20 +2,20 @@
     export let objets = []
 
     $: console.log('[AtelierObjets] objets: ', objets)
+    $: console.log('[visuelObjet2d] objets: ', objets.data[0].attributes.visuelObjet2d.data[0].attributes.url)
 
 </script>
 
 <div>
-    <ul>
-        {#each objets.data as obj}
-            <li>
+    {#each objets.data as obj}
+            <div>
                 <a href="/objets/{obj.id}">
-                    {obj.attributes?.nomObjet}
-
-                    <!-- TODO: Ajout image !
-                        <img src="{obj.attributes.visuelObjet2d?.data[0]?.attributes?.url}" alt=""> -->
+                    {#if obj.attributes?.visuelObjet2d.data }
+                        {#each obj.attributes?.visuelObjet2d.data as visuel}
+                            <img src="{visuel.attributes.url}" alt="{obj.attributes?.nomObjet}"> 
+                        {/each}
+                    {/if}
                 </a>
-            </li>
-        {/each}
-    </ul>
+            </div>
+    {/each}
 </div>
